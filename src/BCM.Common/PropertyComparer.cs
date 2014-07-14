e54b171363af61d34bc3aaed4f493eca0b5c1b14
chstorb_cp <SND\chstorb_cp>
2014-07-14 16:46:56 +0000

    initial checkin

---
+++ src/BCM.Common/PropertyComparer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCM.Common
+{
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Reflection;
+        
+    public class PropertyComparer<T> : Comparer<T>
+    {
+        #region Fields
+
+        private readonly PropertyDescriptor propertyDescriptor;
+        private readonly ListSortDirection sortDirection;
+        private readonly IComparer comparer;
+        private readonly bool userStringComparer = false;
+
+        #endregion Fields
+
+        #region Constructor
+
+        public PropertyComparer(PropertyDescriptor property, ListSortDirection direction)
+        {
+            if (property.ComponentType != typeof(T))
+            {
+                throw new MissingMemberException(typeof(T).Name, property.Name);
+            }
+            if (property.SupportsComparable())
+            {
+                Type t = typeof(Comparer<>).MakeGenericType(property.PropertyType);
+                PropertyInfo propertyInfo = t.GetProperty("Default");
+                this.comparer = (IComparer)propertyInfo.GetValue(null, null);
+            }
+            else if (property.SupportsStringComparer())
+            {
+                this.comparer = StringComparer.CurrentCultureIgnoreCase;
+                this.userStringComparer = true;
+            }
+            this.propertyDescriptor = property;
+            this.sortDirection = direction;
+        }
+
+        #endregion Constructor
+        
+        #region ICompare
+
+        public override int Compare(T x, T y)
+        {
+            object xValue = propertyDescriptor.GetValue(x);
+            object yValue = propertyDescriptor.GetValue(y);
+
+            if (userStringComparer)
+            {
+                xValue = xValue != null ? xValue.ToString() : null;
+                yValue = yValue != null ? yValue.ToString() : null;
+            }
+
+            if (sortDirection == ListSortDirection.Ascending)
+            {
+                return comparer.Compare(xValue, yValue);
+            }
+            else
+            {
+                return comparer.Compare(yValue, xValue);
+            }
+        }
+
+        #endregion ICompare
+    }
+}
---
+++ src/BCM.Common/PropertyDescriptorExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCM.Common
+{
+    using System.ComponentModel;
+    using System.Xml.Linq;
+
+    public static class PropertyDescriptorExtensions
+    {
+        public static bool SupportsComparable(this PropertyDescriptor property)
+        {
+            Type t = property.PropertyType;
+            return (t.GetInterface("IComparable") != null)
+                || (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>));
+        }
+
+        public static bool SupportsStringComparer(this PropertyDescriptor property)
+        {
+            Type t = property.PropertyType;
+            return (t.Equals(typeof(XNode)) || t.IsSubclassOf(typeof(XNode)));
+        }
+        
+        public static bool IsSortable(this PropertyDescriptor property)
+        {
+            return property.SupportsComparable() || property.SupportsStringComparer();
+        }
+    }
+}
