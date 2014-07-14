d5f2b97f757c7ff2415b3e4600192d6cc8fd48ed
chstorb_cp <SND\chstorb_cp>
2014-07-14 20:45:14 +0000

    user control BookDetail, ... added

---
+++ src/BCM.WindowsFormsApplication/BookSelectedEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCM.WindowsFormsApplication
+{
+	public class BookSelectedEventArgs: EventArgs
+	{
+        #region Fields
+	    
+        public int BookId { get; set; }
+        
+        #endregion Fields
+
+        #region Constructor 
+
+		public BookSelectedEventArgs()
+		{
+		}
+
+		public BookSelectedEventArgs(int bookId)
+		{
+			this.BookId = bookId;
+		}
+        
+        #endregion Constructors
+	};
+}
