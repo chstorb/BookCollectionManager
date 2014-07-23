46f09b50593d175d3af4e5dc7b8f005e3fdd5b7f
Christian Storb <chstorb@users.noreply.github.com>
2014-07-23 19:58:50 +0000

    Windows.Forms.Extensions library added

---
+++ src/Windows.Forms.Extensions/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows.Forms.Extensions
+{
+    /// <summary>
+    /// Constants class
+    /// </summary>
+    internal static class Constants
+    {
+        public const char Backspace = '\b';
+
+        public const string InvalidDataSource = "Invalid Data Source!";  
+    }
+}
---
+++ src/Windows.Forms.Extensions/NumericTextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows.Forms.Extensions
+{
+    using System.ComponentModel;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// NumericTextBox class
+    /// </summary>
+    public class NumericTextBox : TextBox
+    {
+        #region Event Handler
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (e.KeyChar == Constants.Backspace)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        #endregion Event Handler
+
+    }
+}
