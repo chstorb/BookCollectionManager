0115ab56de52cb67776516332bff9c415e712b18
Christian Storb <chstorb@users.noreply.github.com>
2014-07-27 14:09:13 +0000

    new tool added

---
+++ tools/ThumbnailRobot/ImageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThumbnailRobot
+{
+    using System.Drawing;
+
+    public static class ImageExtensions
+    {
+        public static bool ThumbnailCallback()
+        {
+            return true;
+        }
+        
+        public static Image ToThumbnail(this Image image)
+        {
+            Image.GetThumbnailImageAbort callback = new Image.GetThumbnailImageAbort(ThumbnailCallback);
+            return image.GetThumbnailImage(100, 100, callback, new IntPtr());                        
+        }
+    }
+}
