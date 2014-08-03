18cef71b9051bae966d9ced896ce9dd14cbf74f3
Christian Storb <chstorb@users.noreply.github.com>
2014-08-03 16:38:28 +0000

    initial check in

---
+++ src/System.Reflection.Extensions/AssemblyExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Reflection.Extensions
+{
+    using System.Reflection;
+
+    /// <summary>
+    /// AssemblyExtensions class
+    /// </summary>
+    public static class AssemblyExtensions
+    {
+        public static string About(this Assembly assembly)
+        {
+            string about = String.Empty;
+            AssemblyProductAttribute productAttribute = assembly.GetCustomAttributes(
+                typeof(AssemblyProductAttribute), true).FirstOrDefault() as AssemblyProductAttribute;
+            if (productAttribute != null)
+            {
+                about = productAttribute.Product.ToString();
+            }
+            Version version = assembly.GetName().Version;
+            if (version != null)
+            {
+                if (about.Length > 0)
+                {
+                    about = String.Format("{0} {1}.{2}.{3}", about, version.Major, version.Minor, version.Build);
+                }
+                else
+                {
+                    about = String.Format("Version {1}.{2}.{3}", version.Major, version.Minor, version.Build);
+                }
+            }
+            return about;
+        }
+    }
+}
